43091881c0d806e5c77be828f6e858d3f978dff2
Jared Rickert <jaredrickert52@gmail.com>
2016-10-06 13:58:55 -0500

    removed unused markdown templates

--- emacs.d/lang/init-markdown.el
+++ emacs.d/lang/init-markdown.el
@@ -3,32 +3,19 @@
 
 ;;; Code:
 (use-package markdown-mode
-  :ensure t
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.markdown\\'" . markdown-mode)
          ("\\.md\\'" . markdown-mode))
-  :config
+  :init
   (let ((markdown-css "https://mirror.jlrickert.me/pandoc-md.css"))
     (setq markdown-command (format "pandoc -s --mathjax --to html --css=%s" markdown-css)))
-  ;; (setq markdown-command "pandoc --from markdown_github-hard_line_breaks --to html")
-  )
-
-(format "%s %s" "a" "b")
-
-(use-package markdown-preview-mode
-  :ensure t
-  :after (markdown-mode)
   :config
-  (add-hook 'markdown-preview-mode-hook
-            (lambda ()
-              (flyspell-mode t)
-              (setq markdown-preview-template
-                    (expand-file-name
-                     "~/.emacs.d/resources/markdown-preview-template.html"
-                     user-emacs-directory))
-              (setq markdown-preview-style
-                    "http://aaronbieber.com/assets/styles/github-markdown.css"))))
+  (add-hook 'markdown-mode-hook
+            '(lambda ()
+               (flyspell-mode t))))
+
+(use-package markdown-preview-mode :after (markdown-mode))
 
 (provide 'init-markdown)
 ;;; init-markdown.el ends here
