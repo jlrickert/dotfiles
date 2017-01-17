8ed5b22a24e8de3dc7f12244a417a6ed2330ab07
Jared Rickert <jaredrickert52@gmail.com>
2017-01-17 16:57:21 -0600

    temporary disable common lisp support

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -88,7 +88,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'init-python)
 (require 'init-lisp)
-(require 'init-common-lisp)
+; (require 'init-common-lisp)
 (require 'init-org)
 (require 'init-clojure)
 (require 'init-haskell)
