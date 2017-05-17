7103c9166a8a55ee18a15da8b93a1e79774e2068
Jared Rickert <jaredrickert52@gmail.com>
2017-05-17 17:52:36 -0500

    no longer try to set users default shell

--- lib/install_oh_my_zsh.sh
+++ lib/install_oh_my_zsh.sh
@@ -67,21 +67,6 @@ main() {
     exit 1
   }
 
-
-  # If this user's login shell is not already "zsh", attempt to switch.
-  TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
-  if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
-    # If this platform provides a "chsh" command (not Cygwin), do it, man!
-    if hash chsh >/dev/null 2>&1; then
-      printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
-      chsh -s $(grep /zsh$ /etc/shells | tail -1)
-    # Else, suggest the user do so manually.
-    else
-      printf "I can't change your shell automatically because this system does not have chsh.\n"
-      printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
-    fi
-  fi
-
   printf "${GREEN}"
   echo '         __                                     __   '
   echo '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
