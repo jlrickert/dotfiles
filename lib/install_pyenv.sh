c69eae6bd89304cdf80d05bdcc445abc7059788a
Jared Rickert <jaredrickert52@gmail.com>
2017-08-07 20:03:11 -0500

    added some niceties for pyenv

--- lib/install_pyenv.sh
+++ lib/install_pyenv.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+git clone https://github.com/yyuu/pyenv-pip-rehash.git ~/.pyenv/plugins/pyenv-rehash
 git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
