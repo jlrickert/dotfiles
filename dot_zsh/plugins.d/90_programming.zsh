4f262335f5ba1f16824faf6ce95406a1200d85f2
jlrickert <jaredrickert52@github.com>
2019-06-24 22:25:56 -0500

    added zsh plugins for several programming languages

---
+++ dot_zsh/plugins.d/90_programming.zsh
@@ -0,0 +1,34 @@
+############################################################
+# Golang config
+############################################################
+export GOPATH=$HOME/golang
+
+path=(
+    $GOPATH/bin
+    $path[@]
+)
+
+############################################################
+# Javascript config
+############################################################
+export npm_config_prefix=~/.node_modules
+
+path=(
+    $HOME/.node_modules/bin
+    $path[@]
+)
+
+
+############################################################
+# Rust config
+############################################################
+RUSTC_BIN=(which rustc)
+if [[ ${?} -eq 0 ]]; then
+  export CARGO_HOME=$HOME/.cargo
+  export RUST_SRC_PATH="$($RUSTC_BIN --print sysroot)/lib/rustlib/src/rust/src"
+
+  path=(
+    $HOME/.cargo/bin
+    $path[@]
+  )
+fi
