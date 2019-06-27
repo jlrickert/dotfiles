cf5922d716e5620948d290a6c840b62c1fba1091
Jared Rickert <jared.rickert@protolabs.com>
2019-06-27 08:45:55 -0500

    no longer requires private key to download public repos from github

--- run_provision.sh
+++ run_provision.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if ! [ -d $HOME/.oh-my-zsh ]; then
-  git clone git@github.com:robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+  git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
 
 if ! [ -d $HOME/.emacs.d ]; then
-  git clone -b develop git@github.com:syl20bnr/spacemacs.git ~/.emacs.d
+  git clone -b develop https://github.com/syl20bnr/spacemacs.git ~/.emacs.d
 fi
