08ba5bf65840855315e62dc1a5556f2b5625d0e6
Jared Rickert <jaredrickert52@gmail.com>
2019-06-28 19:32:05 -0500

    added vundle installation
    added back some vim stuff

--- run_provision.sh
+++ run_provision.sh
@@ -8,7 +8,7 @@ if ! [ -d $HOME/.emacs.d ]; then
   git clone -b develop https://github.com/syl20bnr/spacemacs.git ~/.emacs.d
 fi
 
-if ! [ -d $HOME/.vim ]; then
+if ! [ -d $HOME/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
   vim +PluginInstall +qall
 fi
