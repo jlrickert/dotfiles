1d8916e3434e501bf330a8eb6e164c04ae3a131d
Jared Rickert <jaredrickert52@gmail.com>
2019-12-06 00:38:52 -0600

    added neovim support

--- run_provision.sh
+++ run_provision.sh
@@ -12,3 +12,8 @@ if ! [ -d $HOME/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
   vim +PluginInstall +qall
 fi
+
+if ! [ -d $HOME/.config/nvim/bundle/Vundle.vim ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
+  nvim +PluginInstall +qall
+fi
