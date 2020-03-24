09a8175708a0abf8942639dcff295403027c10eb
Jared Rickert <jaredrickert52@gmail.com>
2020-03-23 22:55:52 -0500

    updated how zsh is handled

---
+++ private_dot_local/private_share/zsh/plugins.d/90_nix.zsh
@@ -0,0 +1,3 @@
+if [ -e /home/jlrickert/.nix-profile/etc/profile.d/nix.sh ]; then
+    . /home/jlrickert/.nix-profile/etc/profile.d/nix.sh;
+fi # added by Nix installer
--- run_provision.sh
+++ run_provision.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if ! [ -d $HOME/.oh-my-zsh ]; then
-  git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+if ! [ -d $HOME/.local/share/oh-my-zsh ]; then
+  git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.local/share/oh-my-zsh
 fi
 
 if ! [ -d $HOME/.emacs.d ]; then
