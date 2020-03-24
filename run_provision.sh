0add5a09e249904852f296198c27e782795fcfc2
Jared Rickert <jaredrickert52@gmail.com>
2020-03-23 23:11:33 -0500

    install Vundle for nvim

--- run_provision.sh
+++ run_provision.sh
@@ -13,7 +13,7 @@ if ! [ -d $HOME/.vim/bundle/Vundle.vim ]; then
   vim +PluginInstall +qall
 fi
 
-if ! [ -d $HOME/.config/nvim/bundle/Vundle.vim ]; then
+if [ -x "(command -v nvim)" ] && [ ! -d $HOME/.config/nvim/bundle/Vundle.vim ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
   nvim +PluginInstall +qall
 fi
