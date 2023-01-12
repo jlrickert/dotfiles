ab38dbb883cdb24fa05efaf803bf7d27c6b130b2
Jared Rickert <jaredrickert52@gmail.com>
2023-01-12 15:59:27 -0600

    Update completion hotkeys

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -485,6 +485,8 @@ require('fidget').setup()
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 
+local select_opts = { behavior = 'select' }
+
 cmp.setup {
     snippet = {
         expand = function(args)
@@ -492,31 +494,28 @@ cmp.setup {
         end,
     },
     mapping = cmp.mapping.preset.insert {
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-5),
+        ['<C-f>'] = cmp.mapping.scroll_docs(5),
         ['<C-Space>'] = cmp.mapping.complete({}),
         ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
+            -- behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         },
-        ['<Tab>'] = cmp.mapping(function(fallback)
+
+        -- navigate items on the list
+        ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+        ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+        ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+        ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+
+        -- toggle completion
+        ['<C-e>'] = cmp.mapping(function()
             if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
+                cmp.abort()
             else
-                fallback()
+                cmp.complete()
             end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
+        end),
     },
     sources = {
         { name = 'nvim_lsp' },
