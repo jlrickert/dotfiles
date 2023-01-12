afa6a337816bafb0f605ee6e36ac9eaea9bb5f47
Jared Rickert <jaredrickert52@gmail.com>
2023-01-12 15:57:15 -0600

    Update diagnostic information
    - Add diagnostic signs in gutter
    - update hover visuals for diagnostics

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -7,6 +7,8 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
     vim.cmd [[packadd packer.nvim]]
 end
 
+local is_vscode = vim.g.vscode
+
 require('packer').startup(function(use)
     -- Package manager
     use 'wbthomason/packer.nvim'
@@ -521,6 +523,55 @@ cmp.setup {
         { name = 'luasnip' },
     },
 }
+
+local setup_diagnostics = function()
+    local sign = function(opts)
+        vim.fn.sign_define(opts.name, {
+            texthl = opts.name,
+            text = opts.text,
+            numhl = ''
+        })
+    end
+
+    sign({ name = 'DiagnosticSignError', text = '✘' })
+    sign({ name = 'DiagnosticSignWarn', text = '▲' })
+    sign({ name = 'DiagnosticSignHint', text = '⚑' })
+    sign({ name = 'DiagnosticSignInfo', text = '' })
+
+    vim.diagnostic.config({
+        virtual_text = false,
+        signs = true,
+        update_in_insert = false,
+        underline = true,
+        severity_sort = true,
+        float = {
+            focusable = false,
+            style = 'minimal',
+            border = 'rounded',
+            source = 'always',
+            header = '',
+            prefix = '',
+        },
+    })
+
+    vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+        vim.lsp.handlers.hover,
+        {
+            border = 'rounded',
+        }
+    )
+
+    vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+        vim.lsp.handlers.signature_help,
+        {
+            border = 'rounded',
+        }
+    )
+end
+
+if not is_vscode then
+    setup_diagnostics()
+end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=4 sts=4 sw=4 et
