cb02f7a0f153a77018344477c1197422cb7d4900
Jared Rickert <jaredrickert52@gmail.com>
2023-10-23 13:40:49 -0500

    feat(nvim): add window picker for file explorer

--- home/dot_config/nvim/lua/plugins/neo_tree.lua
+++ home/dot_config/nvim/lua/plugins/neo_tree.lua
@@ -9,10 +9,22 @@ return {
 			{
 				"s1n7ax/nvim-window-picker",
 				name = "window-picker",
-				-- event = "VeryLazy",
+				event = "VeryLazy",
 				version = "2.*",
 				config = function()
-					require("window-picker").setup()
+					require("window-picker").setup({
+						filter_rules = {
+							include_current_win = false,
+							autoselect_one = true,
+							-- filter using buffer options
+							bo = {
+								-- if the file type is one of following, the window will be ignored
+								filetype = { "neo-tree", "neo-tree-popup", "notify" },
+								-- if the buffer type is one of following, the window will be ignored
+								buftype = { "terminal", "quickfix" },
+							},
+						},
+					})
 				end,
 			},
 		},
