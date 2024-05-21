4dd6b0ff6c565ff49a4f73abbf74a7890674acae
Jared Rickert <jaredrickert52@gmail.com>
2024-05-20 22:38:32 -0500

    feat(nvim): mimic intellij like completions

--- home/dot_config/nvim/lua/plugins/autopairs_and_cmp.lua
+++ home/dot_config/nvim/lua/plugins/autopairs_and_cmp.lua
@@ -43,10 +43,18 @@ return {
 				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-					["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+					["<C-j>"] = cmp.mapping.select_next_item({
+						behavior = cmp.SelectBehavior.Insert, count = 1
+					}), -- next suggestion
+					["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+					["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
 					["<Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
-							cmp.select_next_item()
+							local entry = cmp.get_selected_entry()
+							if not entry then
+								cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+							end
+							cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace })
 						elseif luasnip.expand_or_jumpable() then
 							luasnip.expand_or_jump()
 						else
@@ -54,14 +62,20 @@ return {
 						end
 					end, { "i", "s" }),
 					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.jumpable(-1) then
+						if luasnip.jumpable(-1) then
 							luasnip.jump(-1)
 						else
 							fallback()
 						end
 					end, { "i", "s" }),
+					["<C-y>"] = cmp.mapping(
+						cmp.mapping.confirm {
+							behavior = cmp.ConfirmBehavior.Replace,
+							-- behavior = cmp.ConfirmBehavior.Insert,
+							select = true,
+						},
+						{ "i", "c" }
+					),
 					["<C-u>"] = cmp.mapping.scroll_docs(4), -- scroll up preview
 					["<C-d>"] = cmp.mapping.scroll_docs(-4), -- scroll down preview
 					["<C-Space>"] = cmp.mapping.complete({}), -- show completion suggestions
